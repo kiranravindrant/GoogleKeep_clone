2954003965705df5a82cfe5b207f6ada5d782090
kiranravindrant <kiranravindrant@gmail.com>
2021-10-08 16:24:46 +0530

    [kiran]Added: React js default files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Keep from './components/keep';
+import takenote from './components/takenote';
+// import Signin from './components/signin'
+// import Signup from './components/signup';
+
+
+function App() {
+  return (
+    
+    <div> 
+     
+
+    <Keep/>
+    {/* <Signin/> */}
+    {/* <Signup/> */}
+  
+    
+     </div>
+    
+  );
+}
+
+export default App;
